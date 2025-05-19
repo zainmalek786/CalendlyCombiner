de41e9cefa137e7ef6e0fc85b04b9858e7670978
Zain Akber Malik <zainakbarmalik8@gmail.com>
2025-05-19 10:49:40 -0700

    path updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import store from "./redux/store.js";
 import "./index.css";
 import App from "./App.jsx";
 import CallbackPage from "./components/CallbackPage.jsx";
-import ConnectCalendly from "./components/connectCalendly.jsx";
+import ConnectCalendly from "./components/ConnectCalendly.jsx";
 import LinksForm from "./components/LinksForm.jsx";
 import Error from "./components/Error.jsx";
 import Results from "./components/Results.jsx";
