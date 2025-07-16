17bfd13cf1d4f7fb5b0cf50459fb8d85806a4edf
Zain Akber Malik <zainakbarmalik8@gmail.com>
2025-07-16 05:12:26 -0700

    tailwind error solved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 
 
-
-// vite.config.js
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react()],
 });
